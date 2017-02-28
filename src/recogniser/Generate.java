926a22e21813c13604a7bb165f0fb69fa29e13b3
James Davies <jaymo107@gmail.com>
2017-02-28 19:39:18 +0000

    First

---
+++ src/recogniser/Generate.java
@@ -0,0 +1,27 @@
+package recogniser;
+
+import lib.AbstractGenerate;
+import lib.CompilationException;
+import lib.Token;
+
+/**
+ * @author JamesDavies
+ * @date 28/02/2017
+ * RecursiveDescentRecogniser
+ */
+public class Generate extends AbstractGenerate{
+
+    public Generate() {
+        // Constructor
+    }
+
+    public void reportSuccess() {
+
+    }
+
+    @Override
+    public void reportError(Token token, String explanatoryMessage) throws CompilationException {
+
+    }
+
+}
---
+++ src/recogniser/SyntaxAnalyser.java
@@ -0,0 +1,34 @@
+package recogniser;
+
+import lib.AbstractSyntaxAnalyser;
+import lib.CompilationException;
+
+import java.io.IOException;
+import java.io.PrintStream;
+
+/**
+ * @author JamesDavies
+ * @date 28/02/2017
+ * RecursiveDescentRecogniser
+ */
+public class SyntaxAnalyser extends AbstractSyntaxAnalyser{
+
+   public SyntaxAnalyser(String filename) {
+
+   }
+
+    @Override
+    public void _statementPart_() throws IOException, CompilationException {
+
+    }
+
+    @Override
+    public void acceptTerminal(int symbol) throws IOException, CompilationException {
+
+    }
+
+    public void parse(PrintStream output) {
+
+   }
+
+}
